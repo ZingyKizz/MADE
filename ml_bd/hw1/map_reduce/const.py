22f133a8c6266a4358120692f41e85351a42ab26
Yaroslav Khnykov <zingykizzz@gmail.com>
2021-09-30 17:11:26 +0300

    константы

---
+++ ml_bd/hw1/map_reduce/const.py
@@ -0,0 +1,8 @@
+DATA_INPUT_PATH = "data/AB_NYC_2019.csv"
+RESULT_OUTPUT_PATH = "calculation_result.txt"
+PRICES_OUTPUT_PATH = "data/prices.txt"
+
+PRICE_COLNAME = "price"
+
+DEFAULT_ENCODING = "UTF-8"
+DEFAULT_SEPARATOR = "\n"
