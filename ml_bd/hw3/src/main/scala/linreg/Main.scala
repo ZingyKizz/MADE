f5cb00823a5a6edf8b95f43a0a40604a3e79167b
Yaroslav Khnykov <zingykizzz@gmail.com>
2021-10-22 00:27:12 +0300

    рефакторинг

--- ml_bd/hw3/src/main/scala/linreg/Main.scala
+++ ml_bd/hw3/src/main/scala/linreg/Main.scala
@@ -7,9 +7,7 @@ import play.api.libs.json.Json
 
 
 object Main extends App {
-    val input = new FileInputStream(args(0))
-    val json = Json.parse(input)
-    val cfgData = json.as[InputFile]
+    val cfgData = Json.parse(new FileInputStream(args(0))).as[InputFile]
 
     val x = csvread(new File(cfgData.inputDataPaths.xTrainPath))
     val y = csvread(new File(cfgData.inputDataPaths.yTrainPath)).toDenseVector
@@ -18,10 +16,6 @@ object Main extends App {
     val trainUtils = TrainUtils()
 
     val (xTrain, xVal, yTrain, yVal) = trainUtils.trainValSplit(
-        x, y, shuffle=cfgData.valParams.shuffle, valSize=cfgData.valParams.valSize
-    )
-
-    val trainTuple= trainUtils.trainValSplit(
         x, y, shuffle=cfgData.valParams.shuffle, valSize=cfgData.valParams.valSize
     )
 
