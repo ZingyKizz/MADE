b2bc1a083b8d20ae6a4402489fa989bd1e5c472a
Yaroslav Khnykov <zingykizzz@gmail.com>
2021-10-22 00:53:21 +0300

    добавлен логгер

--- ml_bd/hw3/build.sbt
+++ ml_bd/hw3/build.sbt
@@ -7,5 +7,7 @@ scalaVersion := "2.13.6"
 libraryDependencies ++= Seq(
   "org.scalanlp" %% "breeze" % "1.3",
   "org.scalanlp" %% "breeze-viz" % "1.3",
-  "com.typesafe.play" %% "play-json" % "2.9.2"
+  "com.typesafe.play" %% "play-json" % "2.9.2",
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4"
 )
--- ml_bd/hw3/src/main/scala/linreg/Main.scala
+++ ml_bd/hw3/src/main/scala/linreg/Main.scala
@@ -1,33 +1,48 @@
 package linreg
 
 import breeze.linalg.{csvread, csvwrite}
+import com.typesafe.scalalogging.Logger
 
-import java.io.{File, FileInputStream}
+import java.io.{File, FileInputStream, PrintWriter}
 import play.api.libs.json.Json
 
 
 object Main extends App {
+    val logger = Logger("LinearRegression")
+
+    logger.debug("Loading config...")
     val cfgData = Json.parse(new FileInputStream(args(0))).as[InputFile]
 
+    logger.debug("Reading data...")
     val x = csvread(new File(cfgData.inputDataPaths.xTrainPath))
     val y = csvread(new File(cfgData.inputDataPaths.yTrainPath)).toDenseVector
     val xTest = csvread(new File(cfgData.inputDataPaths.xTestPath))
 
+    logger.debug("Splitting data...")
     val trainUtils = TrainUtils()
 
     val (xTrain, xVal, yTrain, yVal) = trainUtils.trainValSplit(
         x, y, shuffle=cfgData.valParams.shuffle, valSize=cfgData.valParams.valSize
     )
 
+    logger.debug("Fitting model...")
     val linearRegression = LinearRegression()
     linearRegression.fit(
         xTrain, yTrain, learningRate=cfgData.trainParams.learningRate, epochs=cfgData.trainParams.epochs
     )
 
+    logger.debug("Calculating validation score...")
     val yValPred = linearRegression.predict(xVal)
     val valScore = trainUtils.meanSquaredError(yVal, yValPred)
-    println(s"Validation Score: $valScore")
 
+    logger.debug("Writing validation score...")
+    val pw = new PrintWriter(new File(cfgData.outputDataPaths.valScorePath))
+    pw.write(valScore.toString)
+    pw.close()
+
+    logger.debug("Making predictions...")
     val yTestPred = linearRegression.predict(xTest).toDenseMatrix
+
+    logger.debug("Writing Predictions...")
     csvwrite(new File(cfgData.outputDataPaths.yTestPredPath), yTestPred)
 }
